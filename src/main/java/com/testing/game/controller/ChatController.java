14dd117702063e46efa4c57e252e40be7baddc95
Sunny <sunnydeng90@gmail.com>
2019-12-31 00:07:26 -0500

    Changed Controller

--- src/main/java/com/testing/game/controller/ChatController.java
+++ src/main/java/com/testing/game/controller/ChatController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TicTacToeController
+public class ChatController
 {
-    @GetMapping("/tictactoe")
+    @GetMapping("/chat")
     public String getTicTacToe()
     {
-        return "tictactoe";
+        return "chat";
     }
 }
